1e22237eed070d209189d081aa22e508ce5c3da5
tu nombre <emprendehelpacademy@gmail.com>
2024-10-17 13:30:04 -0500

    project-finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ import updateDateYear from './helpers/date_updater.js';
 import loader from './helpers/loader.js';
 import resetToHome from './helpers/reload_page.js';
 import sendEmail from './helpers/send_form.js';
-import changeLang from './helpers/language.js';
+
 
 loader();
 
@@ -18,4 +18,3 @@ updateDateYear();
 
 sendEmail();
 
-changeLang();
